9409ccddbb2c1d68981383e0f72189bddd910412
Nikos katsikanis <nkatsikanis@gmail.com>
2014-08-06 10:32:32 +0100

    added polymer integration

--- app/js/Main.ts
+++ app/js/Main.ts
@@ -23,8 +23,8 @@ angular.module('HTML5SpaceApp')
             var game = new Game.Game();
 
             //game.handleCollisions.bind(game);
-            window.document.addEventListener("keydown", (game.onKeyDown.bind(game)));
-            window.document.addEventListener("keyup", (game.onKeyUp.bind(game)));
+            window.addEventListener("keydown", (game.onKeyDown.bind(game)));
+            window.addEventListener("keyup", (game.onKeyUp.bind(game)));
 
             //$(document).keyup(game.onKeyUp.bind(game));
             setInterval(function () {
