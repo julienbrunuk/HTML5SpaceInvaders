8ac6c559af44aff886f7e81766cd2b79d5e90dfc
Nikos <nkatsikanis@gmail.com>
2014-12-29 13:32:46 +0000

    use requestAnimationFrame

--- app/js/Main.ts
+++ app/js/Main.ts
@@ -8,8 +8,11 @@ var game = new Game.Game();
 window.addEventListener("keydown", (game.onKeyDown.bind(game)));
 window.addEventListener("keyup", (game.onKeyUp.bind(game)));
 
-//$(document).keyup(game.onKeyUp.bind(game));
-setInterval(function () {
-    game.update();
-    game.draw();
-}, 1000 / game.FPS);
+
+function gameLoop() {
+  requestAnimationFrame(gameLoop);
+  // Drawing code goes here
+  game.update();
+  game.draw();
+}
+gameLoop();
